3221a2389b72f90584396e7494f25088c3e030bf
Lapeka <evgenia-m@yandex.ru>
2021-10-06 17:55:28 +0200

    scrooll

--- src/js/script.js
+++ src/js/script.js
@@ -56,5 +56,58 @@ $('.button_mini').each(function(i) {
       $('#order .modal__description').text($('.catalog-item__subtitle').eq(i).text());
       $('.overlay, #order').fadeIn('slow');
   });
+  $('#consultation form').validate({
+    rules: {
+      name: "required",
+      phone: "required",
+      email: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      name: "Пожалуйста, укажите своё имя",
+      phone: "Пожалуйста, укажите номер телефона",
+      email: {
+        required: "Пожалуйста, укажите email",
+        email: "Неправильно введён email"
+      }
+    }
+  });
+  $('#order form').validate();
+  $('#consultation-form').validate();
 });
+
+$('input[name=phone]').mask("+7 (999) 999-99-99");
+
+$('form').submit(function(e) {
+  e.preventDefault();
+  $.ajax({
+    type: "POST",
+    url: "mailer/smart.php",
+    data:$(this).serialize()
+  }).done(function() {
+    $(this).find("input").val("");
+    $('#consultation, #order').fadeOut();
+    $('.overlay, #thanks').fadeIn('slow');
+
+    $('form').trigger('reset');
+  });
+  return false;
+});
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+});
+
+$("a[href^='#']").click(function() {
+  const _href = $(this).attr("href");
+  $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+  return false;
+});
+
 });
